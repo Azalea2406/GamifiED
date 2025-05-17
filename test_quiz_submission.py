59b79f379d20bc802d8580b5a1da20d8988b9769
Ishrath Tabassum <148183636+Azalea2406@users.noreply.github.com>
2025-05-18 02:20:57 +0530

    Update test_quiz_submission.py

--- test_quiz_submission.py
+++ test_quiz_submission.py
@@ -1,1 +1,24 @@
+from Authentication.auth import signup_user, login_user
+from Learning.quiz_submission import submit_quiz
+from Learning.dashboard_page import get_user_xp
+
+print("------ TEST: Sign Up ------")
+signup = signup_user("quizuser@example.com", "securePass123", "student", "quizUser")
+print(signup)
+
+if signup["success"]:
+    print("------ TEST: Login ------")
+    login = login_user("quizuser@example.com", "securePass123")
+    print(login)
+
+    if login["success"]:
+        user_id = login["user_id"]
+
+        print("------ TEST: Submit Quiz ------")
+        result = submit_quiz(user_id, "Python Basics", 0, correct_answers=4)
+        print(result)
+
+        print("------ TEST: Get XP ------")
+        xp = get_user_xp(user_id)
+        print(f"Total XP: {xp}")
 
